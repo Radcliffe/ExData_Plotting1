c655d835c7909f9a69628360eb08144a0e07023d
David Radcliffe <dradcliffe@gmail.com>
2014-11-05 14:19:39 -0600

    Move data into working directory

--- process_data.R
+++ process_data.R
@@ -15,29 +15,30 @@
 ## Check that the dataset exists as a local text file.
 ## Download and unzip the dataset if necessary.
 
-if (!file.exists("../data")) 
-  dir.create("../data")
+if (!file.exists("data")) 
+  dir.create("data")
 
-if (!file.exists("../data/household_power_consumption.txt")) {
+if (!file.exists("data/household_power_consumption.txt")) {
 
-  if (!file.exists("../data/household_power_consumption.zip")) {
+  if (!file.exists("data/household_power_consumption.zip")) {
     download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
-                  destfile="../data/household_power_consumption.zip",
+                  destfile="data/household_power_consumption.zip",
                   method="curl")
   }
   
-  unzip("../data/household_power_consumption.zip", exdir="../data")
+  unzip("data/household_power_consumption.zip", exdir="data")
 }
 
 
 ## Read the data and select the observations from Feb 1 and 2, 2007
 
-power <- read.table("../data/household_power_consumption.txt", 
+power <- read.table("data/household_power_consumption.txt", 
                     sep=";", 
                     header=T, 
                     na.strings="?")
 p <- subset(power, Date == "1/2/2007" | Date == "2/2/2007")
 rm(power)
+file.remove("data/household_power_consumption.txt") #127 MB text file
 
 ## Convert Date and Time fields from strings to dates
 
